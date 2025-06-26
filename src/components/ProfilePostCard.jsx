cb9bf932fbcef5b8730efc76fe3cacf65c0fc8e8
Koki Adachi <kadachi.mys@gmail.com>
2025-06-26 15:25:13 +0800

    Adding/removing likes

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,22 +1,56 @@
-import { Button, Col, Image, Row } from "react-bootstrap";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
+import { Button, Col, Image, Row } from "react-bootstrap";
 
 export default function ProfilePostCard({ content, postId }) {
-  /* Showing likes to the posts */
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState([]);
 
-  /* Fetching likes */
-  useEffect(() => {
-    fetch(
-      `https://db1465cb-65e8-45a7-ad65-e6e95ecaab8e-00-dipkkqto19bh.pike.replit.dev/likes/post/${postId}`
-    )
-      .then((res) => res.json())
-      .then((data) => setLikes(data.length)) //Only getting the number
-      .catch((err) => console.error("Error", err));
-  }, [postId]);
+  // Decoding to get the userId
+  const token = localStorage.getItem("authToken");
+  const decode = jwtDecode(token);
+  const userId = decode.id;
 
   const pic =
     "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+  const BASE_URL =
+    "https://db1465cb-65e8-45a7-ad65-e6e95ecaab8e-00-dipkkqto19bh.pike.replit.dev";
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/likes/post/${postId}`)
+      .then((response) => response.json())
+      .then((data) => setLikes(data))
+      .catch((error) => console.error("Error:", error));
+  }, [postId]);
+
+  const isLiked = likes.some((like) => like.user_id === userId);
+
+  const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
+
+  const addToLikes = () => {
+    axios
+      .post(`${BASE_URL}/likes`, {
+        user_id: userId,
+        post_id: postId,
+      })
+      .then((response) => {
+        setLikes([...likes, { ...response.data, likes_id: response.data.id }]);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const removeFromLikes = () => {
+    const like = likes.find((like) => like.user_id === userId);
+    if (like) {
+      axios
+        .put(`${BASE_URL}/likes/${userId}/${postId}`) // Include userId and postId in the URL
+        .then(() => {
+          // Update the state to reflect the removal of the like
+          setLikes(likes.filter((likeItem) => likeItem.user_id !== userId));
+        })
+        .catch((error) => console.error("Error:", error));
+    }
+  };
 
   return (
     <Row
@@ -32,7 +66,7 @@ export default function ProfilePostCard({ content, postId }) {
 
       <Col>
         <strong>Haris</strong>
-        <span> @haris.samingan • Apr 16</span>
+        <span> @haris.samingan · Apr 16</span>
         <p>{content}</p>
         <div className="d-flex justify-content-between">
           <Button variant="light">
@@ -41,8 +75,13 @@ export default function ProfilePostCard({ content, postId }) {
           <Button variant="light">
             <i className="bi bi-repeat"></i>
           </Button>
-          <Button variant="light">
-            <i className="bi bi-heart"> {likes}</i>
+          <Button variant="light" onClick={handleLike}>
+            {isLiked ? (
+              <i className="bi bi-heart-fill text-danger"></i>
+            ) : (
+              <i className="bi bi-heart"></i>
+            )}
+            {likes.length}
           </Button>
           <Button variant="light">
             <i className="bi bi-graph-up"></i>
