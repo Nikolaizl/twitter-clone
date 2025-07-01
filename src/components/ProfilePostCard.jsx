b8c4552cba5e04dbe8a08b1ba5c633dd421b9dc7
Koki Adachi <kadachi.mys@gmail.com>
2025-07-01 14:48:47 +0800

    Update/delete post

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,20 +1,36 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Button, Col, Image, Row } from "react-bootstrap";
-import { AuthContext } from "./AuthProvider";
 import { useDispatch } from "react-redux";
-import { likePost, removeLikeFromPost } from "../features/posts/postsSlice";
+import {
+  deletePost,
+  likePost,
+  removeLikeFromPost,
+} from "../features/posts/postsSlice";
+import { auth } from "../firebase";
+import UpdatePostModal from "./UpdatePostModal";
 
 export default function ProfilePostCard({ post }) {
-  const { content, id: postId } = post;
-  const [likes, setLikes] = useState([]);
+  const { content, id: postId, imageUrl } = post;
+  // const content = post.content
+  // const postId = post.id
+
+  const [likes, setLikes] = useState(post.likes || []);
+  // const [likes, setLikes] = useState(null || []);
+  // const [likes, setLikes] = useState([]);
+
   const dispatch = useDispatch();
-  const { currentUser } = useContext(AuthContext);
-  const userId = currentUser.uid;
+  const userId = auth.currentUser.uid;
+
+  const isLiked = likes.includes(userId);
 
   const pic =
     "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
 
-  const isLiked = likes.includes(userId);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+
+  const handleShowUpdateModal = () => setShowUpdateModal(true);
+  const handleCloseUpdateModal = () => setShowUpdateModal(false);
+
   const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
 
   const addToLikes = () => {
@@ -25,6 +41,10 @@ export default function ProfilePostCard({ post }) {
   const removeFromLikes = () => {
     setLikes(likes.filter((id) => id !== userId));
     dispatch(removeLikeFromPost({ userId, postId }));
+  };
+
+  const handleDelete = () => {
+    dispatch(deletePost({ userId, postId }));
   };
 
   return (
@@ -38,11 +58,11 @@ export default function ProfilePostCard({ post }) {
       <Col sm={1}>
         <Image src={pic} fluid roundedCircle />
       </Col>
-
       <Col>
         <strong>Haris</strong>
-        <span> @haris.samingan · Apr 16</span>
+        <span> @haris.samingnan · Apr 16</span>
         <p>{content}</p>
+        <Image src={imageUrl} style={{ width: 150 }} />
         <div className="d-flex justify-content-between">
           <Button variant="light">
             <i className="bi bi-chat"></i>
@@ -62,8 +82,20 @@ export default function ProfilePostCard({ post }) {
             <i className="bi bi-graph-up"></i>
           </Button>
           <Button variant="light">
-            <i className="bi bi-upload"></i>
+            <i
+              className="bi bi-pencil-square"
+              onClick={handleShowUpdateModal}
+            ></i>
           </Button>
+          <Button variant="light" onClick={handleDelete}>
+            <i className="bi bi-trash"></i>
+          </Button>
+          <UpdatePostModal
+            show={showUpdateModal}
+            handleClose={handleCloseUpdateModal}
+            postId={postId}
+            originalPostContent={content}
+          />
         </div>
       </Col>
     </Row>
